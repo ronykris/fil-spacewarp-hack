ee9e810ffb26f4676b3bc62a7d9efeaa3c467b82
ronykris <rony.kris@gmail.com>
2023-02-06 23:02:14 +0530

    update

--- filecoinclient.js
+++ filecoinclient.js
@@ -14,20 +14,20 @@ export const startDeal = async() => {
     const lotusClient = new LotusClient(connector)
 
     let params = { Data: {
-                        PieceCid: {'/': 'baga6ea4seaqiancg35wh3bsaez2nzktr36bqgufckk2s5q6cnvto2fzjestxqla'}, 
+                        PieceCid: {'/': 'baga6ea4seaqiancg35wh3bsaez2nzktr36bqgufckk2s5q6cnvto2fzjestxqla'}, //commP
                         PieceSize: 16256, 
                         //Root: {'/': 'bafybeia6dzizisjwkqedds6j4xagxj6d3ek6ddsithpkpes4cosjboojxq'}, 
                         Root: {'/': 'bafykbzacedcb27yfqplrtjemymkf2eu2at25lrgm3fx3mlrqrn3l2ozcnk6su'}, 
                         TransferType: "string"
                     }, 
                     //the startepoch needs to be after [now] + maybe 2h on t01129 or 8h for t01130
-                    DealStartEpoch: 62799, //for t01130; Block Height: 61,711 (from filfox) + 8x120 
+                    DealStartEpoch: 62820, //for t01130; Block Height: 61,711 (from filfox) + 8x120 
                     EpochPrice: '0',
                     FastRetrieval: true, 
                     MinBlocksDuration: 518500, //Duration of epoch, give a very low value to see what the min and max values are for the miner
                     Miner: 't01130', 
                     ProviderCollateral: '0', 
-                    VerifiedDeal: true, 
+                    VerifiedDeal: false, 
                     Wallet: 't1qewhdeyk7hqlepcvnbedapsvie2p3moo6jmymfa'
                     
                 }
